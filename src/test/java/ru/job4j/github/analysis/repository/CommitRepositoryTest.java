c0ccbf1f3d72d28b803df9beb768e0be496d550f
MikhailChrn <mikhail.cherneyev@yandex.ru>
2025-06-21 21:03:44 +0400

    Add repository-layer classes

--- src/test/java/ru/job4j/github/analysis/repository/CommitRepositoryTest.java
+++ src/test/java/ru/job4j/github/analysis/repository/CommitRepositoryTest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CommitRepositoryTest {
 
     @Autowired
-    private RepositoryRepository repositoryRepository;
+    RepositoryRepository repositoryRepository;
 
     @Autowired
     CommitRepository commitRepository;
